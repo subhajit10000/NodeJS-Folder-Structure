ed465dc2110b345bca2206146151f49858d0d279
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-15 19:35:28 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,1 +1,12 @@
 
+// routes/userRoutes.js
+
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/userController");
+
+router.get("/", userController.getUsers);
+router.post("/", userController.createUser);
+
+module.exports = router;
