b909a7f439a9673607dc2d8dc5b5e50f6b1c63b7
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-16 20:04:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
+require("dotenv").config()
 
-const app = require("./app");
+const app = require("./app.js")
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT  || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`server is running at http://localhost:${PORT}`);
+
+})
