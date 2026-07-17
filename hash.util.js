b32d58dadb304336ec14b01ae255ed05b3defc09
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-17 10:05:31 +0530

    Update hash.util.js

--- hash.util.js
+++ hash.util.js
@@ -1,1 +1,10 @@
 
+const bcrypt = require("bcryptjs");
+
+// hashing abc => ksf87r4r2rirgweifgeweig
+const hashedPassword = async (password) => {
+    return await bcrypt.hash(password, 10);
+}
+
+
+module.exports =  { hashedPassword }
