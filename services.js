58b7e2f9d5a748ec30a86d61b1d5761fe6ffc3cf
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-17 10:03:22 +0530

    Implement user registration functionality

--- services.js
+++ services.js
@@ -1,5 +1,33 @@
-const helloguys = () => {
-  return "hello guys";
+const users = require("users.js");
+const {
+    hashedPassword
+} = require("hash.util.js");
+
+
+const registerUser = async (name, email, password) => {
+    const existingUser = users.find((user) => user.email === email);
+    if (existingUser) {
+        throw new Error("User already exists.")
+    }
+
+    // hashing the password.
+
+    const pass = await hashedPassword(password);
+    const newUser = {
+        id: Date.now(),
+        name,
+        email,
+        password: pass
+    }
+
+    users.push(newUser);
+
+    return {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        password: newUser.password
+    }
 };
 
-module.exports = helloguys;
+module.exports = {registerUser};
