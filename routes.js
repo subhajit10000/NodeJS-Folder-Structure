3df4c6e1e6a464c666a8bdcd501cd104ef4bc37d
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-17 10:07:43 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {register} = require("../controllers.js");
+const {register} = require("controllers.js");
 
 const router = express.Router();
 router.post("/register", register)
